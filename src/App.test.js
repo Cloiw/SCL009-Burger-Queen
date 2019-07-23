61810d474ca10ad2a3ede165a027952f875483a9
luisaromero <luisa.marcela.romero@gmail.com>
2019-07-23 17:50:59 -0400

    algunos test

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,11 @@ import Adapter from 'enzyme-adapter-react-16';
 import MenuView from './MenuView'
 import LunchBtn from './MenuView'
 import Btn from './components/Btn'
+import CategoryBtn from './components/CategoryBtn';
 
 configure({ adapter: new Adapter() });
 
-
+// test que verifica si existe el componente
 describe('<MenuView/>', () => {
   test('render', () => {
       const wrapper = shallow(<MenuView/>);
@@ -15,12 +16,31 @@ describe('<MenuView/>', () => {
   });
 
 });
+//test para boton , lo lee pero tira error
 describe('<btn/>', () => {
   test("deberia retorna almuerzos para boton almuerzo" , () => {
-    const wrapper = shallow(<Btn/>);
+    const wrapper = shallow(<CategoryBtn/>);
     wrapper.find(".item-btn").simulate("click");
     expect(onButtonClick).to.have.property('callCount', 1);
     // expect(wrapper.find('').length).toBe(4);
   });
   
+});describe('CategoryBtn', () => {
+  it('Test click event', () => {
+    const mockCallBack = jest.fn();
+
+    const button = shallow((<CategoryBtn onClick={mockCallBack}>Ok!</CategoryBtn>));
+    button.find('.btn').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
 });
+describe('ComponentVisualButton', () => {
+  const clickFn = jest.fn();
+it('debería llamar al evento buttonOnclick', () => {
+  const component = shallow(<CategoryBtn buttonOnClick={clickFn} className="btn" />);
+  component
+    .find('.btn')
+    .simulate('click');
+  expect(clickFn).toHaveBeenCalled();
+})
+})
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './img/logo.png';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as  Link } from "react-router-dom"
 
 const Navbar = () => (
 
