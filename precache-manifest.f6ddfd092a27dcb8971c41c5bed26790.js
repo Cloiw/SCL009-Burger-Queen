0ce1628c8705b630b9b88b5fdb4c73ca631c026d
Cloiw <sircli@hotmail.com>
2019-07-25 16:44:33 -0400

    Updates

---
+++ precache-manifest.f6ddfd092a27dcb8971c41c5bed26790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d074ff95b4826d4a15f22a43b94c2c51",
+    "url": "/SCL009-Burger-Queen/index.html"
+  },
+  {
+    "revision": "4e700fbfce3419689409",
+    "url": "/SCL009-Burger-Queen/static/css/main.06ac79a8.chunk.css"
+  },
+  {
+    "revision": "f90f1876f4c2d9f26408",
+    "url": "/SCL009-Burger-Queen/static/js/2.f8972335.chunk.js"
+  },
+  {
+    "revision": "4e700fbfce3419689409",
+    "url": "/SCL009-Burger-Queen/static/js/main.2f98be20.chunk.js"
+  },
+  {
+    "revision": "e81fd8658db00d469a46",
+    "url": "/SCL009-Burger-Queen/static/js/runtime~main.bfb50733.js"
+  }
+]);
