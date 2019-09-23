b3f88de9178fe1270a9704280460d6f75022e840
Cloiw <sircli@hotmail.com>
2019-09-23 14:07:41 -0300

    arreglando agregados

--- src/views/MenuView.js
+++ src/views/MenuView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {db} from '../data/firebase';
+import { db } from '../data/firebase';
 import '../index.css';
 import Navbar from '../components/Navbar';
 import Btn from '../components/Btn'
@@ -14,7 +14,6 @@ class MenuView extends React.Component{
   
   constructor(props){
     super(props)
-   
     this.state = {list: [], client:"",selectedNavbar:'menu',errorMsg:'',loading:false};
     this.add = this.add.bind(this);
     this.delete = this.delete.bind(this);
@@ -23,83 +22,79 @@ class MenuView extends React.Component{
     this.clearOrder = this.clearOrder.bind(this);
     this.saveOrder = this.saveOrder.bind(this);
     this.addCheeseorEgg = this.addCheeseorEgg.bind(this);
-    this.index = 0; // id de cada elemento de orden creado
-   
+    this.index = 0; 
   }
-  //añade un producto({name,value,id} a la orden(list -> array de objetos [el index comienza en 0 y se utiliza como id]
+
   add(valueToAdd,nameToAdd){
     let newList = [...this.state.list] 
-          for( let i = 0; i < newList.length; i++){ 
-            if ( newList[i].name === nameToAdd) {
-                newList[i].count++;
-                newList[i].value = newList[i].value + valueToAdd;
-              return this.setState({list:newList})
-            }
-          }
+    for( let i = 0; i < newList.length; i++) { 
+      if (newList[i].name === nameToAdd) {
+        newList[i].count++;
+        newList[i].value = newList[i].value + valueToAdd;
+        return this.setState({list:newList})
+      }
+    }
     this.setState({list: this.state.list.concat([{name : nameToAdd, unit_value:valueToAdd, value:valueToAdd, id:this.index, count:1 }])});
     this.index ++;
   }
 
-  addCheeseorEgg(eggOrCheese){
+  addCheeseorEgg(eggOrCheese) {
     let newList = [...this.state.list] 
     let lastItem = newList[newList.length-1];
-    if(lastItem ===undefined){
+    const lastItemAdd = lastItem.name.substr(lastItem.name.length - 7);
+
+    if(lastItem === undefined || lastItemAdd === '+ Queso' || lastItemAdd === '+ Huevo') {
       return
     }
-    if ( eggOrCheese === '+ Queso') {
-    
-              lastItem.name = lastItem.name+" + Queso"
-              lastItem.value = lastItem.value + 500;
-              return this.setState({list:newList})
+    if (eggOrCheese === '+ Queso') {
+      lastItem.name = lastItem.name+' + Queso';
+      lastItem.value = lastItem.value + (lastItem.count * 500);
+      return this.setState({list:newList})
     }
-              lastItem.name = lastItem.name+" + Huevo"
-              lastItem.value = lastItem.value + 500;
-             this.setState({list:newList})
+    lastItem.name = lastItem.name+' + Huevo';
+    lastItem.value = lastItem.value + (lastItem.count * 500);
+    this.setState({list:newList})
   }
   
-
-  //
-  delete(id){
-    let newList = [...this.state.list] //crea copia de list para no cambiar directamente el estado de un componente
-    for( var i = 0; i < newList.length; i++){ //recorre la newList y consigue la id de cada elemento,si coincide
+  delete(id) {
+    let newList = [...this.state.list] 
+    for( var i = 0; i < newList.length; i++) { 
       if ( newList[i].id === id) { 
-        if( newList[i].count !== 1){
+        if( newList[i].count !== 1) {
           newList[i].value = newList[i].value - newList[i].unit_value ;
           newList[i].count--;
           return this.setState({list:newList})
         }
-        newList.splice(i, 1); //con la misma id que se le pasa borra este elemento (desde posicion i, elimina 1 elemento)
+        newList.splice(i, 1);
         break;
       }
     }
-   this.setState({list:newList}) //remplaza la list con la newList modificada
+    this.setState({list:newList})
   }
 
-
-  view(category){ // dependiendo de la categoria cambia el estado, este metodo se pasa como propiedad al boton
-    category = category.toLowerCase()
-      
-    if(category ==="desayunos"){
+  view(category) {
+    category = category.toLowerCase();
+    if(category === 'desayunos'){
       this.setState({
         desayunos:true,
         almuerzos:false
-        })
+      })
     }
-    if(category ==="almuerzos"){
+    if(category === 'almuerzos') {
       this.setState({
         desayunos:false,
         almuerzos:true
-        })
-      }
+      })
     }
+  }
 
-  changeClient(el){
+  changeClient(el) {
     this.setState({
       client: el.target.value
     })
   }
 
-  clearOrder(){
+  clearOrder() {
     this.setState({
       list: [],
       client:"",
@@ -110,15 +105,15 @@ class MenuView extends React.Component{
   saveOrder(){
     if(this.state.client === ""){
       this.setState({
-        errorMsg:'Debes ingresar un nombre.'}) 
+        errorMsg:'Debes ingresar un nombre.'
+      }) 
       return 
     }
     this.setState({
       loading:true
     })
-    let idClient =this.state.client + Date.now();
-
-    let data=
+    let idClient = this.state.client + Date.now();
+    let data =
     {
     id: idClient,
     client: this.state.client,
@@ -127,7 +122,7 @@ class MenuView extends React.Component{
     time: Date.now() 
     }
 
-    db.collection("ordenes").doc(idClient).set(data)
+    db.collection('ordenes').doc(idClient).set(data)
     .then(() => {
       this.setState({
         loading:false
@@ -143,42 +138,44 @@ class MenuView extends React.Component{
     return (
       <>
         <Navbar state={this.state.selectedNavbar} />
-          <div className="content-row">
-            <section className="button-content-col">
-              <div className="category-btn-row">
-                {Object.keys(Menu).map(btn=> <CategoryBtn  className ="btnCategory" name={btn.toUpperCase()} state={btn === 'Desayunos' ? this.state.desayunos : this.state.almuerzos} view={this.view} key={btn}/>)}
-              </div>
-                {this.state.desayunos &&
-                <div className="item-btn-row"> {Menu.Desayunos.map(btn=><Btn name={btn.name} value={btn.value} add={this.add} key={btn.name}/>)}
-                </div> }
-                {this.state.almuerzos && <LunchBtn add={this.add} addCheeseorEgg={this.addCheeseorEgg}/>}  
-            </section>  
-            <aside className="side-content-col">
+        <div className="content-row">
+          <section className="button-content-col">
+            <div className="category-btn-row">
+              {Object.keys(Menu).map(btn=> <CategoryBtn  className ="btnCategory" name={btn.toUpperCase()} state={btn === 'Desayunos' ? this.state.desayunos : this.state.almuerzos} view={this.view} key={btn}/>)}
+            </div>
+              {this.state.desayunos &&
+              <div className="item-btn-row"> 
+              {Menu.Desayunos.map(btn=><Btn name={btn.name} value={btn.value} add={this.add} key={btn.name}/>)}
+              </div> }
+              {this.state.almuerzos && <LunchBtn add={this.add} addCheeseorEgg={this.addCheeseorEgg}/>}  
+          </section>  
+          <aside className="side-content-col">
             <div className="aside-content">
               <div className="line-order">
                 <p>PEDIDO</p>
                 <p className='error-msg'>{this.state.errorMsg}</p>
               </div>
-              
               <OrderName changeClient={this.changeClient} client={this.state.client}/>
               <div className="order-content">
                 <Order list = {this.state.list} delete={this.delete}/>
               </div>
-              
-                {!this.state.loading  ?
-                <footer className="footer-side">
+              {!this.state.loading  ?
+              <footer className="footer-side">
                 <div className="btn-clear-col">
                   <button className="btn-aside-clear" onClick={this.clearOrder}>LIMPIAR</button>
                 </div>
                 <div className="btn-send-col">
                   <button className="btn-aside" onClick={this.saveOrder}>ENVIAR </button>
-                </div> </footer>: <footer><p>Cargando . . .</p></footer> }
-                
-                
-              
-              </div>
-            </aside>
-          </div>
+                </div>
+              </footer>
+              : 
+              <footer>
+                <p>Cargando . . .</p>
+              </footer>
+              }
+            </div>
+          </aside>
+        </div>
       </>
     )
   }
